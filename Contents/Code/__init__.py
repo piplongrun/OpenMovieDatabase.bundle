6a989fc300fb7ca7009e72530d551909bb717c59
Sander <sander@plex.tv>
2014-11-12 23:38:42 +0100

    Rewrite: - Make compatible with The Movie Database agent; - Add more checks before using certain data; - Add option to add IMDb and Rotten Tomatoes score to summary field; - Make every piece of metadata optional through the agent's preferences.

--- Contents/Code/__init__.py
+++ Contents/Code/__init__.py
@@ -1,82 +1,123 @@
+API_URL = 'http://www.omdbapi.com/?i=%s&plot=%s&tomatoes=true'
 RE_RUNTIME = Regex('((?P<hours>[0-9]+) hrs? )?(?P<minutes>[0-9]+) min')
 
 def Start():
+
   HTTP.CacheTime = CACHE_1WEEK
-  HTTP.Headers['User-Agent'] = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_8_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/31.0.1650.63 Safari/537.36'
+  HTTP.Headers['User-Agent'] = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10) AppleWebKit/600.1.25 (KHTML, like Gecko) Version/8.0 Safari/600.1.25'
   HTTP.Headers['Referer'] = 'http://www.imdb.com/'
 
-class UnofficialImdbApi(Agent.Movies):
+class OmdbApi(Agent.Movies):
+
   name = 'Open Movie Database'
   languages = [Locale.Language.English]
   primary_provider = False
-  contributes_to = ['com.plexapp.agents.imdb']
+  contributes_to = [
+    'com.plexapp.agents.imdb',
+    'com.plexapp.agents.themoviedb'
+  ]
 
   def search(self, results, media, lang):
-    imdb_id = media.primary_metadata.id
-    results.Append(MetadataSearchResult(id=imdb_id, score=99))
+
+    if media.primary_agent == 'com.plexapp.agents.imdb':
+
+      imdb_id = media.primary_metadata.id
+
+    elif media.primary_agent == 'com.plexapp.agents.themoviedb':
+
+      # Get the IMDb id from the Movie Database Agent
+      imdb_id = Core.messaging.call_external_function(
+        'com.plexapp.agents.themoviedb',
+        'MessageKit:GetImdbId',
+        kwargs = dict(
+          tmdb_id = media.primary_metadata.id
+        )
+      )
+
+      if not imdb_id:
+        Log("*** Could not find IMDb id for movie with The Movie Database id: %s ***" % (media.primary_metadata.id))
+        return None
+
+    results.Append(MetadataSearchResult(
+      id = imdb_id,
+      score = 100
+    ))
 
   def update(self, metadata, media, lang):
-    plot = 'full'
-    if Prefs['imdb_plot_short']:
-      plot = 'short'
 
-    url = 'http://www.omdbapi.com/?i=%s&plot=%s&tomatoes=true' % (metadata.id, plot)
+    url = API_URL % (metadata.id, Prefs['plot'].lower())
 
     try:
       movie = JSON.ObjectFromURL(url, sleep=5.0)
     except:
-      Log('Failed when trying to open url: ' + url)
-      movie = None
+      Log('*** Failed when trying to open url: %s ***' % (url))
+      return None
 
-    if movie is not None:
-      if movie['Response'] == 'True':
+    if 'Response' in movie and movie['Response'] == 'True':
+
+      # Title
+      if Prefs['use_title'] and 'Title' in movie:
         metadata.title = movie['Title']
+      else:
+        metadata.title = None
+
+      # Year
+      if Prefs['use_year'] and 'Year' in movie:
         metadata.year = int(movie['Year'])
+      else:
+        metadata.year = None
 
-        if movie['Rated'] != 'N/A':
-          metadata.content_rating = movie['Rated']
-        else:
-          metadata.content_rating = None
+      # Plot
+      if Prefs['use_plot'] and 'Plot' in movie and movie['Plot'] != 'N/A':
+        metadata.summary = movie['Plot']
+      else:
+        metadata.summary = None
 
-        if movie['Released'] != 'N/A':
-          metadata.originally_available_at = Datetime.ParseDate(movie['Released']).date()
-        else:
-          metadata.originally_available_at = None
+      # Content rating
+      if Prefs['use_content_rating'] and 'Rated' in movie and movie['Rated'] != 'N/A':
+        metadata.content_rating = movie['Rated']
+      else:
+        metadata.content_rating = None
 
-        metadata.genres.clear()
-        if movie['Genre'] != 'N/A':
-          for genre in movie['Genre'].split(','):
-            metadata.genres.add(genre.strip())
+      # Release date
+      if Prefs['use_release_date'] and 'Released' in movie and movie['Released'] != 'N/A':
+        metadata.originally_available_at = Datetime.ParseDate(movie['Released']).date()
+      else:
+        metadata.originally_available_at = None
 
-        metadata.writers.clear()
-        if movie['Writer'] != 'N/A':
-          for writer in movie['Writer'].split(','):
-            metadata.writers.add(writer.rsplit('(', 1)[0].strip())
+      # Genres
+      metadata.genres.clear()
 
-        metadata.directors.clear()
-        if movie['Director'] != 'N/A':
-          for director in movie['Director'].split(','):
-            metadata.directors.add(director.rsplit('(', 1)[0].strip())
+      if Prefs['use_genres'] and 'Genre' in movie and movie['Genre'] != 'N/A':
+        for genre in movie['Genre'].split(','):
+          metadata.genres.add(genre.strip())
 
-        metadata.roles.clear()
-        if movie['Actors'] != 'N/A':
-          for actor in movie['Actors'].split(','):
-            role = metadata.roles.new()
-            role.actor = actor.strip()
+      # Directors
+      metadata.directors.clear()
 
-        if movie['Plot'] != 'N/A':
-          metadata.summary = movie['Plot']
-        else:
-          metadata.summary = ''
+      if Prefs['use_directors'] and 'Director' in movie and movie['Director'] != 'N/A':
+        for director in movie['Director'].split(','):
+          metadata.directors.add(director.rsplit('(', 1)[0].strip())
 
-        if Prefs['rating'] == 'Rotten Tomatoes' and 'tomatoRating' in movie and movie['tomatoRating'] != 'N/A':
-          metadata.rating = float(movie['tomatoRating'])
-        elif Prefs['rating'] == 'IMDb' and 'imdbRating' in movie and movie['imdbRating'] != 'N/A':
-          metadata.rating = float(movie['imdbRating'])
-        else:
-          metadata.rating = None
+      # Writers
+      metadata.writers.clear()
 
+      if Prefs['use_writers'] and 'Writer' in movie and movie['Writer'] != 'N/A':
+        for writer in movie['Writer'].split(','):
+          metadata.writers.add(writer.rsplit('(', 1)[0].strip())
+
+      # Actors
+      metadata.roles.clear()
+
+      if Prefs['use_actors'] and 'Actors' in movie and movie['Actors'] != 'N/A':
+        for actor in movie['Actors'].split(','):
+          role = metadata.roles.new()
+          role.actor = actor.strip()
+
+      # Runtime
+      if Prefs['use_runtime'] and 'Runtime' in movie:
         duration = 0
+
         try:
           runtime = RE_RUNTIME.search(movie['Runtime']).groups()
           if 'hours' in runtime:
@@ -85,26 +126,64 @@ class UnofficialImdbApi(Agent.Movies):
             duration += int(runtime['minutes']) * 60 * 1000
         except:
           pass
+
         if duration > 0:
           metadata.duration = duration
-
-        if Prefs['imdb_poster'] and movie['Poster'] != 'N/A':
-          try:
-            poster = movie['Poster']
-            fullsize = poster.split('@@')[0] + '@@._V1._SX640.jpg'
-            thumb    = poster.split('@@')[0] + '@@._V1._SX100.jpg'
-            if fullsize not in metadata.posters:
-              p = HTTP.Request(thumb)
-              metadata.posters[fullsize] = Proxy.Preview(p, sort_order=1)
-          except:
-            pass
         else:
-          try:
-            poster = movie['Poster']
-            del metadata.posters[poster]
-          except:
-            pass
+          metadata.duration = None
 
       else:
-        Log('An error occured. The json response is:')
-        Log(movie)
+        metadata.duration = None
+
+      # Poster
+      if Prefs['use_poster'] and 'Poster' in movie and movie['Poster'] != 'N/A':
+
+        try:
+          poster = movie['Poster']
+          fullsize = poster.split('@@')[0] + '@@._V1._SX640.jpg'
+          thumb    = poster.split('@@')[0] + '@@._V1._SX100.jpg'
+
+          if fullsize not in metadata.posters:
+            p = HTTP.Request(thumb)
+            metadata.posters[fullsize] = Proxy.Preview(p, sort_order=1)
+
+        except:
+          pass
+
+      else:
+        metadata.posters.validate_keys([])
+
+      # Rating
+      if Prefs['use_rating']:
+
+        rating_imdb = None
+        rating_rt = None
+
+        if 'imdbRating' in movie and movie['imdbRating'] != 'N/A':
+          rating_imdb = movie['imdbRating']
+
+        if 'tomatoMeter' in movie and movie['tomatoMeter'] != 'N/A':
+          rating_rt = movie['tomatoMeter']
+
+        if Prefs['rating'] == 'IMDb' and rating_imdb:
+          metadata.rating = float(rating_imdb)
+        elif Prefs['rating'] == 'Rotten Tomatoes' and rating_rt:
+          metadata.rating = float(int(rating_rt)/10)
+
+        summary = [metadata.summary]
+
+        if Prefs['add_rating_rt']:
+          summary.append('Rotten Tomatoes: %s%%' % (rating_rt))
+
+        if Prefs['add_rating_imdb']:
+          summary.append('IMDb: %s' % (rating_imdb))
+
+        summary.reverse()
+        metadata.summary = ' | '.join(summary)
+
+      else:
+        metadata.rating = None
+
+    else:
+      Log('*** Failed when processing data from url: %s ***' % (url))
+      return None
