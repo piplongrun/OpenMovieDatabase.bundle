923860f6aa6f0a9843b2b9a9b4bfe05575a63625
piplongrun <piplongrun@users.noreply.github.com>
2017-05-25 03:32:05 +0200

    Update __init__.py

--- Contents/Code/__init__.py
+++ Contents/Code/__init__.py
@@ -4,7 +4,7 @@ API_URL = 'https://api.tadata.me/omdb/v1/?imdb_id=%s'
 
 def Start():
 
-  HTTP.CacheTime = CACHE_1WEEK
+  HTTP.CacheTime = 0
   HTTP.Headers['User-Agent'] = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.12.4) AppleWebKit/603.1.30 (KHTML, like Gecko) Version/10.1 Safari/603.1.30'
   HTTP.Headers['Referer'] = 'http://www.imdb.com/'
 
@@ -53,7 +53,7 @@ class OmdbApi(Agent.Movies):
     url = API_URL % (metadata.id)
 
     try:
-      movie = JSON.ObjectFromURL(url, sleep=5.0)
+      movie = JSON.ObjectFromURL(url, sleep=1.0)
     except:
       Log('*** Failed when trying to open url: %s ***' % (url))
       return None
@@ -61,31 +61,31 @@ class OmdbApi(Agent.Movies):
     if not 'error' in movie:
 
       # Title
-      if Prefs['use_title'] and 'title' in movie:
+      if Prefs['use_title'] and movie['title']:
         metadata.title = movie['title']
       else:
         metadata.title = None
 
       # Year
-      if Prefs['use_year'] and 'year' in movie:
+      if Prefs['use_year'] and movie['year']:
         metadata.year = movie['year']
       else:
         metadata.year = None
 
       # Plot
-      if Prefs['use_plot'] and 'plot' in movie:
+      if Prefs['use_plot'] and movie['plot']:
         metadata.summary = movie['plot']
       else:
         metadata.summary = None
 
       # Content rating
-      if Prefs['use_content_rating'] and 'rated' in movie:
+      if Prefs['use_content_rating'] and movie['rated']:
         metadata.content_rating = movie['rated']
       else:
         metadata.content_rating = None
 
       # Release date
-      if Prefs['use_release_date'] and 'released' in movie:
+      if Prefs['use_release_date'] and movie['released']:
         metadata.originally_available_at = Datetime.ParseDate(movie['released']).date()
       else:
         metadata.originally_available_at = None
@@ -93,12 +93,12 @@ class OmdbApi(Agent.Movies):
       # Genres
       metadata.genres.clear()
 
-      if Prefs['use_genres'] and 'genres' in movie:
+      if Prefs['use_genres'] and movie['genres']:
         for genre in movie['genres']:
           metadata.genres.add(genre.strip())
 
       # Production company
-      if Prefs['use_production'] and 'studio' in movie:
+      if Prefs['use_production'] and movie['studio']:
         metadata.studio = movie['studio']
       else:
         metadata.studio = None
@@ -106,7 +106,7 @@ class OmdbApi(Agent.Movies):
       # Directors
       metadata.directors.clear()
 
-      if Prefs['use_directors'] and 'directors' in movie:
+      if Prefs['use_directors'] and movie['directors']:
         for director in movie['directors']:
           try:
             meta_director = metadata.directors.new()
@@ -120,7 +120,7 @@ class OmdbApi(Agent.Movies):
       # Writers
       metadata.writers.clear()
 
-      if Prefs['use_writers'] and 'writers' in movie:
+      if Prefs['use_writers'] and movie['writers']:
         for writer in movie['writers']:
           try:
             meta_writer = metadata.writers.new()
@@ -134,7 +134,7 @@ class OmdbApi(Agent.Movies):
       # Actors
       metadata.roles.clear()
 
-      if Prefs['use_actors'] and 'actors' in movie:
+      if Prefs['use_actors'] and movie['actors']:
         for actor in movie['actors']:
           role = metadata.roles.new()
           try:
@@ -146,7 +146,7 @@ class OmdbApi(Agent.Movies):
               pass
 
       # Runtime
-      if Prefs['use_runtime'] and 'runtime' in movie:
+      if Prefs['use_runtime'] and movie['runtime']:
          metadata.duration = movie['runtime'] * 1000
       else:
         metadata.duration = None
@@ -154,7 +154,7 @@ class OmdbApi(Agent.Movies):
       # Poster
       valid_names = list()
 
-      if Prefs['use_poster'] and 'poster' in movie:
+      if Prefs['use_poster'] and movie['poster']:
 
         fullsize = '%s.jpg' % (movie['poster'].rsplit('_', 1)[0])
         thumb = '%s_SX300.jpg' % (movie['poster'].rsplit('_', 1)[0])
@@ -178,7 +178,7 @@ class OmdbApi(Agent.Movies):
         rating_rt = None
 
         if 'imdb' in movie['ratings']:
-          rating_imdb = movie['ratings']['imdb']
+          rating_imdb = float(movie['ratings']['imdb'])
 
         if 'rt' in movie['ratings']:
           rating_rt = movie['ratings']['rt']
